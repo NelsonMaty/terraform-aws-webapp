021b611069067122e6eeef056ff775c636658d54
Nelson M. Rios <nelsonmatias9@hotmail.com>
2025-01-26 17:20:22 -0300

    feat: customize index page for apache

--- infrastructure/setup_apache.sh
+++ infrastructure/setup_apache.sh
@@ -2,3 +2,6 @@
 sudo yum update -y
 sudo yum install -y httpd.x86_64
 sudo systemctl enable httpd --now
+
+# Create index.html with Hello World message
+echo "<html><body><h1>Hello World from AWS</h1></body></html>" | sudo tee /var/www/html/index.html
