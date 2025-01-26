9aa11fb7980cb71af502ffbef7cc0a48ebc1d940
Nelson M. Rios <nelsonmatias9@hotmail.com>
2025-01-26 17:30:49 -0300

    chore: changing file to test gh action

--- infrastructure/setup_apache.sh
+++ infrastructure/setup_apache.sh
@@ -3,5 +3,4 @@ sudo yum update -y
 sudo yum install -y httpd.x86_64
 sudo systemctl enable httpd --now
 
-# Create index.html with Hello World message
 echo "<html><body><h1>Hello World from the cloud!</h1></body></html>" | sudo tee /var/www/html/index.html
