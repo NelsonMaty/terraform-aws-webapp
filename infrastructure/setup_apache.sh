80490e4f88bfb52de29b6a8e39442bda5281146e
Nelson M. Rios <nelsonmatias9@hotmail.com>
2025-01-26 17:22:12 -0300

    chore: testing gh workflow

--- infrastructure/setup_apache.sh
+++ infrastructure/setup_apache.sh
@@ -4,4 +4,4 @@ sudo yum install -y httpd.x86_64
 sudo systemctl enable httpd --now
 
 # Create index.html with Hello World message
-echo "<html><body><h1>Hello World from AWS</h1></body></html>" | sudo tee /var/www/html/index.html
+echo "<html><body><h1>Hello World from the cloud!</h1></body></html>" | sudo tee /var/www/html/index.html
