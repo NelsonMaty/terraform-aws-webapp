8b54615a72ebf7d56fb9c70811eb0090f7cf8731
Nelson M. Rios <nelsonmatias9@hotmail.com>
2025-02-01 13:52:46 -0300

    chore: Remove static HTML content from Apache setup script

--- infrastructure/setup_apache.sh
+++ infrastructure/setup_apache.sh
@@ -2,5 +2,3 @@
 sudo yum update -y
 sudo yum install -y httpd.x86_64
 sudo systemctl enable httpd --now
-
-echo "<html><body><h1>Hello World from mundose!</h1></body></html>" | sudo tee /var/www/html/index.html
